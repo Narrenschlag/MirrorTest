98c89c49ed237c2d55aec67a0dc32752e7cc26a6
Maximilian Schecklmann <narrenschlag@gmail.com>
2021-05-28 12:22:45 +0200

    Initial Unity Project

---
+++ Assets/Code/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+using Mirror;
+
+public class Player : NetworkBehaviour
+{
+    [Header("Movement")]
+    public float MOVEMENT_SPEED = .5f;
+
+    private void Update()
+    {
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
+        if (isLocalPlayer)
+        {
+            float _moveX = Input.GetAxis("Horizontal");
+            float _moveY = Input.GetAxis("Vertical");
+
+            Vector3 _move = new Vector3(_moveX, 0, _moveY);
+            transform.position += _move * MOVEMENT_SPEED * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    //
+    //Jojojo was geht
+
+    private void Start()
+    {
+        Debug.Log("Srrrrrr.");
+        GameObject _client = new GameObject();
+
+        print("JHasdasd");
+    }
+
+    private void Update()
+    {
+        Debug.Log("Chill mal Bruder");
+    }
+}
